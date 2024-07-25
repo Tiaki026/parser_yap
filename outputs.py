981526fdfd7351662678b27162ed4cf439cec0af
Eugene Kolotikov <jeka.pxb@yandex.ru>
2024-07-25 17:49:01 +0300

    out

--- outputs.py
+++ outputs.py
@@ -1,30 +1,32 @@
 # outputs.py
 from prettytable import PrettyTable
+from constants import BASE_DIR
+
+
+def file_output(results, cli_args):
+    results_dir = BASE_DIR / 'results'
+    results_dir.mkdir(exist_ok=True)
+    pass
 
 
 def control_output(results, cli_args):
-    if cli_args.pretty:
-        # Вывод в формате PrettyTable.
+    output = cli_args.output
+    if output == 'pretty':
         pretty_output(results)
+    elif output == 'file':
+        file_output(results, cli_args)
     else:
-        # Вывод по умолчанию. 
         default_output(results)
 
 
 def default_output(results):
-    # Печатаем список results построчно.
     for row in results:
-        print(*row)    
+        print(*row)
 
 
 def pretty_output(results):
-    # Инициализируем объект PrettyTable.
     table = PrettyTable()
-    # В качестве заголовков устанавливаем первый элемент списка.
     table.field_names = results[0]
-    # Выравниваем всю таблицу по левому краю.
     table.align = 'l'
-    # Добавляем все строки, начиная со второй (с индексом 1).
     table.add_rows(results[1:])
-    # Печатаем таблицу.
     print(table)
